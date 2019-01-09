c9055bd4f0f452756e5341391d839a673b1c96eb
ahborba <alexandrehanczuruk@gmail.com>
2019-01-09 13:44:01 +0000

    final

--- src/video/thread/ThreadPrompt.java
+++ src/video/thread/ThreadPrompt.java
@@ -21,11 +21,13 @@ public class ThreadPrompt extends Thread {
 
 	public void run() {
 		String msg = "";
-		boolean transmissao = false;
-		System.out.println("Iniciando ThreadPrompt");
+		System.out.println("Iniciando Thread do Prompt");
 
 		while (sc.hasNextLine()) {
 			msg = sc.nextLine().trim();
+			
+
+			
 			
 			switch (msg.substring(0, 2)) {
 			case "10":
@@ -33,37 +35,31 @@ public class ThreadPrompt extends Thread {
 					cliente = new Cliente(ip, msg.substring(2, 3));
 	
 				break;
-			case "sair":
-				System.exit(0);
-				break;
 			case "12":
 				if(cliente != null) {
 					cliente.encerrar();
-				
 				}
 				break;
 			case "11":
 			case "13":
-				enviar(msg);
+				try (Socket socket = new Socket(this.ip, 6060);
+						PrintStream out = new PrintStream(socket.getOutputStream());
+						Scanner in = new Scanner(socket.getInputStream());){
+					out.println(msg);
+					out.flush();
+					System.out.println(in.nextLine().trim());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			default:
 				break;
 			}
 
 		}
 
-		System.out.println("Finalizando ThreadPrompt");
+		System.out.println("Finalizando Thread do Prompt");
 
 	}
 
-	private void enviar(String msg) {
-		try (Socket socket = new Socket(this.ip, 6060);
-				PrintStream out = new PrintStream(socket.getOutputStream());
-				Scanner in = new Scanner(socket.getInputStream());){
-			out.println(msg);
-			out.flush();
-			System.out.println(in.nextLine().trim());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+
 }
